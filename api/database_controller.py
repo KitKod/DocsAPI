5bcd7cd3386db75d92c6e482cd49a8464058229e
Mykyta Kamak <mykytakamak@gmail.com>
2021-07-20 22:29:08 +0300

    Add get, put and update methods

--- api/database_controller.py
+++ api/database_controller.py
@@ -1,19 +1,34 @@
-import os, datetime
+import datetime
+import os
 
+from pydantic import BaseModel
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date
+
+
+class DocsItem(BaseModel):
+    name: str
+    content: str
+    owner: str
 
 
 class DocsTableManipulator:
 
     def __init__(self):
+        self.column_names = [
+            'id',
+            'name',
+            'content',
+            'owner',
+            'creation_date',
+        ]
         self.meta = MetaData()
         self.table = Table(
             'docs', self.meta,
-            Column('id', Integer, primary_key = True),
-            Column('name', String),
-            Column('content', String),
-            Column('owner', String),
-            Column('creation_date', Date, default = datetime.date.today),
+            Column(self.column_names[0], Integer, primary_key = True),
+            Column(self.column_names[1], String),
+            Column(self.column_names[2], String),
+            Column(self.column_names[3], String),
+            Column(self.column_names[4], Date, default = datetime.date.today),
         )
         self.engine = create_engine(
             'postgresql://{user}:{pwd}@{host}/{db}'.format(user = os.environ['DB_USERNAME'],
@@ -24,11 +39,34 @@ class DocsTableManipulator:
         )
         self.meta.create_all(self.engine)
 
-    def add_docs(self, name, content, owner):
+    def add_docs(self, doc_item):
         with self.engine.connect() as conn:
-            ins = self.table.insert().values(name = name, content = content, owner = owner)
-            result = conn.execute(ins)
-            print(result)
+            ins = self.table.insert().values(name = doc_item.name,
+                                             content = doc_item.content,
+                                             owner = doc_item.owner)
+            conn.execute(ins)
 
     def get_doc(self, id):
-        pass
+        with self.engine.connect() as conn:
+            selected = self.table.select().where(self.table.c.id == id)
+            result = conn.execute(selected)
+
+        return self.__parse_query_result(result)
+
+    def update_doc(self, id, doc_item):
+        with self.engine.connect() as conn:
+            to_update = {k: v for k, v in doc_item.dict().items() if v}
+            updated = self.table.update().where(self.table.c.id == id).values(**to_update)
+            conn.execute(updated)
+
+    def delete_doc(self, id):
+        with self.engine.connect() as conn:
+            deleted = self.table.delete().where(self.table.c.id == id)
+            conn.execute(deleted)
+
+    def __parse_query_result(self, result):
+        parsed_data = []
+        for row in result:
+            parsed_data.append({x: y for x, y in zip(self.column_names, row)})
+
+        return parsed_data
--- api/main.py
+++ api/main.py
@@ -1,22 +1,26 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
-from database_controller import DocsTableManipulator
+from database_controller import DocsTableManipulator, DocsItem
 
 app = FastAPI()
+docs_table = DocsTableManipulator()
 
 
-@app.get("/")
-def home():
-    docs_table = DocsTableManipulator()
-    docs_table.add_docs("kitkod1", "3", "4")
-    return {"Hello": "Ratibor"}
+@app.post("/documents/add_doc")
+def add_doc(doc_item: DocsItem):
+    docs_table.add_docs(doc_item)
 
-@app.get("/docs/{doc_id}")
+
+@app.get("/documents/{doc_id}")
 def get_doc_by_id(doc_id: int):
-
-    return {}
-    
+    return docs_table.get_doc(doc_id)[0]
 
 
+@app.put("/documents/{doc_id}")
+def update_doc_by_id(doc_id: int, doc_item: DocsItem):
+    return docs_table.update_doc(doc_id, doc_item)
+
+
+@app.delete("/documents/{doc_id}")
+def delete_doc_by_id(doc_id: int):
+    return docs_table.delete_doc(doc_id)
